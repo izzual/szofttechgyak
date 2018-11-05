7c26e114d58dc25773c8cdf5de326ea75c90fc4c
Peter Csapo <peter.csapo14@gmail.com>
2018-11-05 14:04:13 +0100

    added gitignore

---
+++ NorthMinta/HomeLap.aspx
@@ -0,0 +1,10 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/NorthSite.Master" AutoEventWireup="true" CodeBehind="HomeLap.aspx.cs" Inherits="NorthMinta.HomeLap" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+    <h1>Üdvözöl a Northwind adatbázis</h1>
+    <p>Ez a weboldal a Northwind adatbázishoz való hozzáférést mutatja be.
+        Néhány alaművelettel táblákat mutatunk meg, szűréseket végzünk.
+    </p>
+    <p>Az adatbázis műveleteket típusos DataSet és Entity Framework használatával mutatjuk be.</p>
+</asp:Content>
---
+++ NorthMinta/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("NorthMinta")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("HP Inc.")]
+[assembly: AssemblyProduct("NorthMinta")]
+[assembly: AssemblyCopyright("Copyright © HP Inc. 2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("ed0cbead-0d2b-44ec-8e27-a0c8f9d50c60")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers 
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
