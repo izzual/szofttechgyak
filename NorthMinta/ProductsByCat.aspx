f5c40f8503f6a096522d52b7a227708c29dd4719
Peter Csapo <peter.csapo14@gmail.com>
2018-11-05 14:26:18 +0100

    added code to pageload and selectindexchanged

--- NorthMinta/ProductsByCat.aspx
+++ NorthMinta/ProductsByCat.aspx
@@ -2,4 +2,20 @@
 <asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
 </asp:Content>
 <asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+    <asp:DropDownList ID="DropKategoria" runat="server" AppendDataBoundItems="True" AutoPostBack="True" OnSelectedIndexChanged="DropKategoria_SelectedIndexChanged" Width="160px">
+        <asp:ListItem Value="-1">(Válasszon Kategóriát)</asp:ListItem>
+    </asp:DropDownList>
+    <asp:GridView ID="GridProducts" runat="server" CellPadding="4" ForeColor="#333333" GridLines="None">
+        <AlternatingRowStyle BackColor="White" ForeColor="#284775" />
+        <EditRowStyle BackColor="#999999" />
+        <FooterStyle BackColor="#5D7B9D" Font-Bold="True" ForeColor="White" />
+        <HeaderStyle BackColor="#5D7B9D" Font-Bold="True" ForeColor="White" />
+        <PagerStyle BackColor="#284775" ForeColor="White" HorizontalAlign="Center" />
+        <RowStyle BackColor="#F7F6F3" ForeColor="#333333" />
+        <SelectedRowStyle BackColor="#E2DED6" Font-Bold="True" ForeColor="#333333" />
+        <SortedAscendingCellStyle BackColor="#E9E7E2" />
+        <SortedAscendingHeaderStyle BackColor="#506C8C" />
+        <SortedDescendingCellStyle BackColor="#FFFDF8" />
+        <SortedDescendingHeaderStyle BackColor="#6F8DAE" />
+    </asp:GridView>
 </asp:Content>
--- NorthMinta/ProductsByCat.aspx.cs
+++ NorthMinta/ProductsByCat.aspx.cs
@@ -7,11 +7,32 @@ using System.Web.UI.WebControls;
 
 namespace NorthMinta {
     public partial class ProductsByCat : System.Web.UI.Page {
-        protected void Page_Load(object sender, EventArgs e) {
 
+        private NorthwindEntities entities = new NorthwindEntities();
+        protected void Page_Load(object sender, EventArgs e) {
+            if (!this.IsPostBack) {
+                DropKategoria.DataSource = entities.Categories.ToList();
+                DropKategoria.DataTextField = "CategoryName";
+                DropKategoria.DataValueField = "CategoryID";
+                DropKategoria.DataBind();
+            }
         }
 
         protected void DropKategoria_SelectedIndexChanged(object sender, EventArgs e) {
+            int selectedId = Int32.Parse(DropKategoria.SelectedValue);
+            if(selectedId == -1) {
+                GridProducts.DataSource = null;
+            } else {
+                var products = from product in entities.Products
+                               where product.CategoryID == selectedId
+                               select new {
+                                   Name = product.ProductName,
+                                   Quantity = product.QuantityPerUnit,
+                                   Stock = product.UnitsInStock
+                               };
+                GridProducts.DataSource = products.ToArray();
+            }
+            GridProducts.DataBind();
 
         }
     }
