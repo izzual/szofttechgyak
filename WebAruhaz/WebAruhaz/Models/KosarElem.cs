a591a64648cb81b00c360458cc7caaa9934565a7
Peter Csapo <peter.csapo14@gmail.com>
2018-11-26 14:31:19 +0100

    commit message #3 ?

---
+++ WebAruhaz/WebAruhaz/Models/KosarElem.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace WebAruhaz.Models {
+    public class KosarElem {
+        [Key]
+        public string ElemId { get; set; }
+
+        public string KosarId { get; set; }
+
+        public int Mennyiseg { get; set; }
+
+        public DateTime LetrehozasDatum { get; set; }
+
+        public int BicikliId { get; set; }
+        public virtual Bicikli Bicikli { get; set; }
+    }
+}
