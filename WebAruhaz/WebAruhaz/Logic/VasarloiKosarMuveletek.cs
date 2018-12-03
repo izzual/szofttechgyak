df4e2732d06a96d992ae3416724fd5ef51bde3c9
Peter Csapo <peter.csapo14@gmail.com>
2018-12-03 13:55:01 +0100

    edited formatting

--- WebAruhaz/WebAruhaz/Logic/VasarloiKosarMuveletek.cs
+++ WebAruhaz/WebAruhaz/Logic/VasarloiKosarMuveletek.cs
@@ -4,10 +4,8 @@ using System.Linq;
 using System.Web;
 using WebAruhaz.Models;
 
-namespace WebAruhaz.Logic
-{
-    public class VasarloiKosarMuveletek : IDisposable
-    {
+namespace WebAruhaz.Logic {
+    public class VasarloiKosarMuveletek : IDisposable {
         public string VasarloiKosarId { get; set; }
         private BicikliContext db = new BicikliContext();
         public const string KosarSessionKey = "KosarId";
@@ -18,13 +16,11 @@ namespace WebAruhaz.Logic
             var kosarElem = (from c in db.VasarloiKosarElemek
                              where c.ElemId == VasarloiKosarId && c.BicikliId == id
                              select c).SingleOrDefault();
-            if (kosarElem==null)
-            {
+            if (kosarElem == null) {
                 //Létrehozunk egy új kosárelem objektumot, ha még nem létezett egysem.
                 //Mivel nincs paraméteres az osztálynak
                 // inicializálással adunk értéket a tulajdonságoknak
-                kosarElem = new KosarElem
-                {
+                kosarElem = new KosarElem {
                     ElemId = Guid.NewGuid().ToString(),
                     BicikliId = id,
                     KosarId = VasarloiKosarId,
@@ -35,9 +31,7 @@ namespace WebAruhaz.Logic
                     LetrehozasDatuma = DateTime.Now
                 };
                 db.VasarloiKosarElemek.Add(kosarElem);
-            }
-            else
-            {
+            } else {
                 //Ha az elem már létezik a kosárban, akkor megnöveljük 1-el 
                 // a mennyiségét
                 kosarElem.Mennyiseg++;
@@ -45,32 +39,26 @@ namespace WebAruhaz.Logic
             db.SaveChanges(); // Lementjük a változásokat az adatbázisba
         }
 
-        private string GetKosarId()
-        {
+        private string GetKosarId() {
             // Kosár Session azonosítójának beállítása
-            if (HttpContext.Current.Session[KosarSessionKey]==null)
-            {
+            if (HttpContext.Current.Session[KosarSessionKey] == null) {
                 if (!string.IsNullOrWhiteSpace
                     (HttpContext.Current.User.Identity.Name)) //aktuális felhasználó neve
                 {
                     HttpContext.Current.Session[KosarSessionKey] =
                         HttpContext.Current.User.Identity.Name;
-                }
-                else
-                {
+                } else {
                     //Létrehozunk egy új véletlen GUID értéket a System.GUID osztály segítségével
                     Guid tempKosarId = Guid.NewGuid();
-                    HttpContext.Current.Session[KosarSessionKey] = 
+                    HttpContext.Current.Session[KosarSessionKey] =
                         tempKosarId.ToString();
                 }
             }
             return HttpContext.Current.Session[KosarSessionKey].ToString();
         }
 
-        public void Dispose()
-        {
-            if (db!=null)
-            {
+        public void Dispose() {
+            if (db != null) {
                 db.Dispose();
                 db = null;
             }
