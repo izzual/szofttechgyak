5cc48145a9109f4916ec3b9351be24526a01df9a
Peter Csapo <peter.csapo14@gmail.com>
2018-12-03 13:41:20 +0100

    added VasarloiKosar codebehind

--- WebAruhaz/WebAruhaz/VasarloiKosar.aspx.cs
+++ WebAruhaz/WebAruhaz/VasarloiKosar.aspx.cs
@@ -4,14 +4,18 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using WebAruhaz.Models;
+using WebAruhaz.Logic;
 
-namespace WebAruhaz
-{
-    public partial class VasarloiKosar : System.Web.UI.Page
-    {
-        protected void Page_Load(object sender, EventArgs e)
-        {
+namespace WebAruhaz {
+    public partial class VasarloiKosar : System.Web.UI.Page {
+        protected void Page_Load(object sender, EventArgs e) {
 
+        }
+
+        public List<KosarElem> GetVasarloiKosarElemek() {
+            VasarloiKosarMuveletek muveletek = new VasarloiKosarMuveletek();
+            return muveletek.GetKosarElemek();
         }
     }
 }
