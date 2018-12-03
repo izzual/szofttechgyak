a3d8aef5a20f2c4c9b36ff138230692badec8b69
Peter Csapo <peter.csapo14@gmail.com>
2018-12-03 14:06:00 +0100

    added kosarOsszesen logic

--- WebAruhaz/WebAruhaz/VasarloiKosar.aspx.cs
+++ WebAruhaz/WebAruhaz/VasarloiKosar.aspx.cs
@@ -11,6 +11,17 @@ namespace WebAruhaz {
     public partial class VasarloiKosar : System.Web.UI.Page {
         protected void Page_Load(object sender, EventArgs e) {
 
+            using (VasarloiKosarMuveletek usersVasarloiKosar = new VasarloiKosarMuveletek()) {
+                decimal kosarOsszesen = 0;
+                kosarOsszesen = usersVasarloiKosar.GetOsszesen();
+                if (kosarOsszesen > 0) {
+                    lblTotal.Text = String.Format("{0:c}", kosarOsszesen);
+                } else {
+                    LabelTotalText.Text = "";
+                    lblTotal.Text = "";
+                    VasarloiKosarCim.InnerText = "A vaásárlói (sic) kosár üres!";
+                }
+            }
         }
 
         public List<KosarElem> GetVasarloiKosarElemek() {
